590a4865aa9064970761aa6d0095983973ca853d
Erick <ericklucio-suv@hotmail.com>
2020-08-29 14:31:38 -0300

    .

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
     statusCode: 200,
-    body: JSON.stringify({
+    body: JSON({
       message: "Hello there",
       status:"Can you hear me?"
     }),
