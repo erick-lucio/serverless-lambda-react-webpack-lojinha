0dc21ef16adf94de951372b4bcc57bb3deec047f
Erick <ericklucio-suv@hotmail.com>
2020-08-30 20:39:21 -0300

    .

--- functions/todos-delete.js
+++ functions/todos-delete.js
@@ -2,10 +2,16 @@
 const faunadb = require('faunadb')
 const getId = require('./utils/getId')
 const q = faunadb.query
+/*
+exports.handler = async event =>{
 
+${subject}
+*/
 
 exports.handler = async (event, context) => {
   /* configure faunaDB Client with our secret */
+  const subject = event.queryStringParameters.name || "world"
+  console.log("subject - :",subject)
   const client = new faunadb.Client({
     secret: process.env.SECRET_KEY_FAUNA
   }) 
