3861581755683367e951e32a2d86f8b9c5bbcf8d
Erick <ericklucio-suv@hotmail.com>
2020-08-30 02:13:48 -0300

    .

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-const dbConfig = require("../config/database");
-const Products = require("../models/Products");
-const connection = new Sequelize(dbConfig);
-
-Products.init(connection);
-
-module.exports = connection;
