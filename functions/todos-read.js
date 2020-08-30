e4a7ff36e897510a597fd75b2ec9bf90701b1c91
Erick <ericklucio-suv@hotmail.com>
2020-08-30 20:58:03 -0300

    .

--- functions/todos-read.js
+++ functions/todos-read.js
@@ -5,14 +5,15 @@ const q = faunadb.query
 
 exports.handler = (event, context) => {
   /* configure faunaDB Client with our secret */
+  console.log("event path : "+event.path)
   const subject = event.queryStringParameters.name || "world"
   console.log("subject - : " + subject)
   const client = new faunadb.Client({
     secret: process.env.SECRET_KEY_FAUNA
   }) 
-  const id = getId(event.path)
-  console.log(`Function 'todo-read' invoked. Read id: ${id}`)
-  return client.query(q.Get(q.Ref(`classes/todos/${id}`)))
+  //const id = getId(event.path)
+  console.log(`Function 'todo-read' invoked. Read id: 2}`)
+  return client.query(q.Get(q.Ref(`classes/todos/2`)))
     .then((response) => {
       console.log('success', response)
       return {
