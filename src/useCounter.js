630d7ab388e974e80133a78bf376ba3f1199b99e
Erick <ericklucio-suv@hotmail.com>
2020-09-02 11:41:14 -0300

    .

--- src/useCounter.js
+++ src/useCounter.js
@@ -2,7 +2,7 @@
 import React, { useState ,useReducer} from "react";
 
 import {reducerCart,reducerProducts,reducerCounter,estadoInicialCart,estadoInicialCounter,estadoInicialProducts} from './Redux'
-export function useCounter(){
+function useCounter(){
   
     const [stateCounter, dispatchCounter] = useReducer(reducerCounter,estadoInicialCounter); 
     const increment = () =>{
