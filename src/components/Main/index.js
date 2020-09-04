72661a0d75d4b32b6d336b333e0559304b48f8fd
Erick <ericklucio-suv@hotmail.com>
2020-09-04 10:43:11 -0300

    .

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -18,7 +18,7 @@ const Main = () => {
     //Da um get  e pega o id de produtos e o joga pro reducer
     fetch('/.netlify/functions/read-all-products').
     then((response)=>{
-      console.log(response.json)
+      console.log(response.json())
     })
     
     dispatchProducts({type:'Clear'})
