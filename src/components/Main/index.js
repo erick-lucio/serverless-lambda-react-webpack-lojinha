6858496e41bde37ae594a6bcc7be2dd1c3298da1
Erick <ericklucio-suv@hotmail.com>
2020-09-05 18:25:44 -0300

    .

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -75,7 +75,7 @@ const Main = () => {
             
           <ProductDiv key={key}>
              
-            <img src={import("../../assets/imgs/img_dress.jpeg")} style={{height: "40%",width: 120}}></img>
+            <img src={obj.img_path} style={{height: "40%",width: 120}}></img>
             <Text fontsize={2} style={{marginTop:10,overflowWrap:"anywhere",textAlign:"center",height:"40%"}}>
                {obj.name}
             </Text>
