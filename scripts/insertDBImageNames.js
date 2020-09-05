200c670099a288bd526ea40060ed5469f75eda42
Erick <ericklucio-suv@hotmail.com>
2020-09-05 19:02:12 -0300

    .

--- scripts/insertDBImageNames.js
+++ scripts/insertDBImageNames.js
@@ -4,7 +4,7 @@ export const getImagesNames = () => {
     let importedImgs = []
     // return an array list of filenames (with extension)
     const importAll = (r) => r.keys().map(file => file.match(/[^\/]+$/)[0]);
-    importAll(r).array.forEach(element => {
+    importAll(r).forEach(element => {
         let productData = {    
             product_name: "Roupa ",
             price: Math.floor(Math.random() * (500 - 59 + 1)) + 59,
