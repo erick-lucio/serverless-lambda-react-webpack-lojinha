988f5c40a3aaa8d9e779c3857d60df60d3a758e9
Erick <ericklucio-suv@hotmail.com>
2020-09-05 03:41:18 -0300

    .

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -73,7 +73,7 @@ const Main = () => {
         
         return (            
           <ProductDiv key={key}>
-            <img url={'../../assets/imgs'+images[obj.img_path].default} style={{height: "40%",width: 120}}></img>
+            <img url={'/.netlify/src/assets/imgs/'+images[obj.img_path].default} style={{height: "40%",width: 120}}></img>
             <Text fontsize={2} style={{marginTop:10,overflowWrap:"anywhere",textAlign:"center",height:"40%"}}>
                {obj.name}
             </Text>
