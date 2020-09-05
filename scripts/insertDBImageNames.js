d3f822b80b7335a7f29a4f1361870a31f5ceeca0
Erick <ericklucio-suv@hotmail.com>
2020-09-05 18:58:38 -0300

    .

--- scripts/insertDBImageNames.js
+++ scripts/insertDBImageNames.js
@@ -1,12 +1,12 @@
 
 export const getImagesNames = () => {
-    const r = require.context('./assets/imgs', false, /\.(jpe?g|png|gif|svg)$/);
+    const r = require.context('../src/assets/imgs', false, /\.(jpe?g|png|gif|svg)$/);
     let importedImgs = []
     // return an array list of filenames (with extension)
     const importAll = (r) => r.keys().map(file => file.match(/[^\/]+$/)[0]);
     importAll(r).array.forEach(element => {
         let productData = {    
-            product_name: "Roupa Braba",
+            product_name: "Roupa ",
             price: Math.floor(Math.random() * (500 - 59 + 1)) + 59,
             img_name: element
         }
