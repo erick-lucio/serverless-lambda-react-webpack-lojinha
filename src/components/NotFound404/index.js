f53c1e2004f56b9af8ee308d7a90eb8d726d6df8
Erick <ericklucio-suv@hotmail.com>
2020-09-06 18:30:32 -0300

    .

--- src/components/NotFound404/index.js
+++ src/components/NotFound404/index.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Container, ProductDiv, TextDiv, Text } from "./styles";
-
+import { useHistory } from "react-router-dom";
 const NotFound = (props) => {
+  let history = useHistory();
   return (
     <Container>
       <ProductDiv>
         <TextDiv>
           <Text fontsize={4}>Pagina Não Encontrada</Text>
+          <Text fontsize={3} onClick={()=>history.push("/")}>Voltar ao menu principal</Text>
         </TextDiv>
       </ProductDiv>
     </Container>
