97188b9835ab84d2bc9da1987f0162ec5dd111dd
Erick <ericklucio-suv@hotmail.com>
2020-09-06 17:12:24 -0300

    .

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -61,7 +61,7 @@ const Product = (props) => {
     });
   };
 
-  if ((id_param = !undefined)) {
+  if ((id_param =! undefined)) {
     if (product == undefined) {
       return (
         <Container>
